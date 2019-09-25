fd0dd6dd1ddc1a996f176931025697aca1ba0756
klouskingsley <harry_tse@163.com>
2019-09-25 15:39:25 +0800

    1 file modified

--- src/app/comment/comment-http.entity.ts
+++ src/app/comment/comment-http.entity.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core'
 import {Subject} from 'rxjs'
+import {CommentItem} from './comment.d'
+import {COMMENT_DB_KEY, COMMENT_MESSAGE_TYPE} from './comment.config'
 
 
 /**
@@ -11,19 +13,34 @@ import {Subject} from 'rxjs'
 })
 export class CommentHttpEntity {
 
-  private saveDB () {
-
+  private saveDB (commentList: CommentItem[]) {
+    localStorage.setItem(COMMENT_DB_KEY, JSON.parse(commentList))
   }
 
-  private getDB () {
-
+  private getDB (): CommentItem[] {
+    const data = localStorage.getItem(COMMENT_DB_KEY)
+    if (data) {
+      return JSON.parse(data)
+    }
+    return []
   }
 
-  sendComment (content: string, userName: string): Promise<number> {
-    
+  sendComment (content: string, username: string): Promise<number> {
+    const id = Date.now() + Math.random()
+    const commentList = this.getDB()
+    const comment = {
+      id,
+      content,
+      username,
+    }
+    commentList.unshift(comment)
+    this.saveDB(commentList)
+    return Promise.resolve(id)
   }
 
-  deleteComment (commentId: number): Promise<void> {  }
+  deleteComment (commentId: number): Promise<void> {
+
+  }
 
   clearComment (): Promise<void> {  }
 
